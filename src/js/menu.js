aa432a1d67527a81cf47b5d7b22ae5c4d5493a5c
Egor Plyatsko <mr.error406@gmail.com>
2021-08-29 18:06:17 +0300

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,16 +1,10 @@
-// import menuTemplate from '../templates/menu.hbs';
-// import menuMath from '../menu.json'
-
-
-// const menu = menuTemplate(menuMath);
-// // console.log(menuTemplate(menuMath))
-// const menuRef = document.querySelector('.js-menu');
-// menuRef.insertAdjacentHTML('beforeend', menu);
-
-import menu from '../menu.json'
 import itemsTemplate from '../templates/menu.hbs'
+import menu from '../menu.json'
 
 const galleryRef = document.querySelector('.js-menu')
 const markup = itemsTemplate(menu)
-console.log(itemsTemplate(menu))
+
 galleryRef.insertAdjacentHTML('beforeend', markup)
+
+
+
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,20 +1,26 @@
-
-
-
 const switchToggle = document.querySelector('#theme-switch-toggle')
 const bodyRef = document.querySelector("body")
+const startTheme = localStorage.getItem('siteTheme')
+
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 }
 
+localStorage.getItem('siteTheme') ? bodyRef.classList.add(startTheme):''
+    if (startTheme == Theme.LIGHT) {
+       switchToggle.checked = true
+    }
+
 if(localStorage.getItem("theme")){
-const checked = JSON.parse(localStorage.getItem("theme"))
+    const checked = JSON.parse(localStorage.getItem("theme"))
     switchToggle.checked = checked
     switchToggle.checked
         ? onDarkTheme()
         : onLightTheme()
-}
+    }
+
+
 
 function checkboxState(event) {
     localStorage.setItem("theme", JSON.stringify(event.target.checked))
@@ -32,5 +38,7 @@ function onLightTheme() {
     bodyRef.classList.add(Theme.LIGHT)
     bodyRef.classList.remove(Theme.DARK)
 }
+
+
 
 switchToggle.addEventListener('change', checkboxState)
